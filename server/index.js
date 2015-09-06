08c61c1b7a2db68c0f93aac2afc686b13e5fc904
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2015-09-06 09:20:44 -0300

    Heroku config added. Also the listening port has been changed from 3000 to 5000

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+var cool = require("cool-ascii-faces");
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -6,6 +7,8 @@ var _ = require('lodash');
 
 // Create the application.
 var app = express();
+
+app.set('port', (process.env.PORT || 5000))
 
 // Add Middleware necessary for REST API's
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +21,10 @@ app.use(function(req, res, next){
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
+});
+
+app.get('/cool', function(req, res){
+	res.send(cool());
 });
 
 /*app.use('/hello', function(req, res, next){
@@ -39,6 +46,8 @@ mongoose.connection.once('open', function(){
 		app.use(route, controller(app, route));
 	});
 	
-	console.log('Listening on port 3000...');
-	app.listen(3000);
+	//console.log('Listening on port 5000...');
+	app.listen(app.get('port'), function() {
+		console.log('Node app is running on port', app.get('port'));
+	});
 });
